f3a82a24e8f16afba37163fe58cf5fdf41f64d28
neswill1 <nenadkonatar@live.ac.me>
2015-04-17 10:15:31 +0200

    futer

--- first/index.php
+++ first/index.php
@@ -115,7 +115,7 @@
 		    <h1><img src="img/logo.png" alt="LOGO"></h1>
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
-            <h1>Powered by <a href="http://mobilegrows.com/">MobileGrows </a>|<a href="http://www.williamjdvorakdds.com/" rel="external"> Desktop Site</a></h1>
+            <h1>Powered by <a href="http://mobilegrows.com/" rel="external" >MobileGrows </a>|<a href="http://www.williamjdvorakdds.com/" rel="external"> Desktop Site</a></h1>
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
